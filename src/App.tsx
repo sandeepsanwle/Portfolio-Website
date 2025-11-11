af2bfe3a4e82f40a9819f53573025cbb4359fa03
sandeep_sanwle <sandeepsanwle74@gmail.com>
2025-11-11 22:32:32 +0530

    Fix GitHub Pages 404 error - Switch to HashRouter and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Toaster } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -9,12 +9,12 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <Toaster />
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>
 );
 
